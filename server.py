bd00abbe5abf6acc0d5692002a0193368caf4b5e
Grimmick <soumikpradhan16@gmail.com>
2016-11-24 06:06:18 +0530

    basic server and client added
    A simple listen server and a client.

--- server.py
+++ server.py
@@ -11,5 +11,5 @@ s.listen(5)                 # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
    print('Got connection from', addr)
-   c.send('Thank you for connecting')
+   c.send(b'Thank you for connecting')
    c.close()                # Close the connection
